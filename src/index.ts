8671362c787c97bb237a2ca2517469f1f5f4189c
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-10-07 20:03:12 +0700

    feat(day 2): add get all users from db

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import db from "./db";
+import { Request, Response } from "express";
 
 dotenv.config();
 
@@ -12,7 +13,6 @@ app.get("/", (_, res) => {
     res.json({ message: "Hello, World!" });
 });
 
-import { Request, Response } from "express";
 
 const getAllUsers = async (_: Request, response: Response) => {
     try {
