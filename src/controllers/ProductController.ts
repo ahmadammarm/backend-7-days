88201a1a74702cf1b283df6c9d612ec4da6f6d57
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-10-10 11:13:58 +0700

    refactor(day 5): adjust get all products functionallity with authenticated user

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,15 +1,23 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import db from "../db";
 
-const GetAllProducts = async (_: any, response: Response, next: NextFunction) => {
+const GetAllProducts = async (request: Request, response: Response, next: NextFunction) => {
     try {
-        const products = await db.query('SELECT * FROM products');
+        const userId = request.user?.id;
+
+        if (!userId) {
+            return response.status(401).json({ error: "Unauthorized" });
+        }
+
+        const products = await db.query('SELECT * FROM products WHERE user_id = $1', [userId]);
+
         response.status(200).json({
             code: 200,
             success: true,
             message: 'Products retrieved successfully',
             data: products.rows,
         });
+        
     } catch (error: any) {
         next(error);
     }
