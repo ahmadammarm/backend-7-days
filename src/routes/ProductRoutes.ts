1124478100b38cd07b32e25bd19af23be2b21002
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-10-25 07:57:02 +0700

    feat: add product create and edit validator

--- src/routes/ProductRoutes.ts
+++ src/routes/ProductRoutes.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 import { CreateProduct, DeleteProductById, EditProductById, GetAllProducts, GetProductById } from "../controllers/ProductController";
 import IsAuthenticated from "../middlewares/IsAuthenticated";
 import FileUpload from "../middlewares/FileUpload";
+import { CreateProductValidator, EditProductValidator } from "../validators/ProductValidator";
+import { Validate } from "../middlewares/Validate";
 
 const ProductRouter = Router();
 
 ProductRouter.get("/products", IsAuthenticated, GetAllProducts);
-ProductRouter.post("/products", IsAuthenticated, FileUpload.single('image'), CreateProduct);
+ProductRouter.post("/products", IsAuthenticated, FileUpload.single('image'), CreateProductValidator, Validate, CreateProduct);
 ProductRouter.get("/products/:id", IsAuthenticated, GetProductById);
-ProductRouter.put("/products/:id", IsAuthenticated, FileUpload.single('image'), EditProductById);
+ProductRouter.put("/products/:id", IsAuthenticated, FileUpload.single('image'), EditProductValidator, Validate, EditProductById);
 ProductRouter.delete("/products/:id", IsAuthenticated, DeleteProductById);
 
 export default ProductRouter;
